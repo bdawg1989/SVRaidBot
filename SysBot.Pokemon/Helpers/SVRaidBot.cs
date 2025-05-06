dadce6056187a601619b3a03958bb2ad29b010de
Taku1991 <Takuta1991@outlook.at>
2025-05-06 07:18:11 +0200

    Update Version

--- SysBot.Pokemon/Helpers/SVRaidBot.cs
+++ SysBot.Pokemon/Helpers/SVRaidBot.cs
@@ -2,8 +2,8 @@
 {
     public static class SVRaidBot
     {
-        public const string Version = "v8.4.2";
-        public const string Repo = "https://github.com/bdawg1989/SVRaidBot";
+        public const string Version = "v1.1.0";
+        public const string Repo = "https://github.com/Taku1991/SVRaidBot";
         public const string ConfigPath = "config.json";
     }
 }
